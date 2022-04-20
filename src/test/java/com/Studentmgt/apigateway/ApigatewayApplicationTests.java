6b021c338c978829c0186253d3b49eaeff91670b
Siva <lakshmi.kalagatla@gmail.com>
2022-04-20 14:49:24 -0400

    First change

---
+++ src/test/java/com/Studentmgt/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Studentmgt.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
